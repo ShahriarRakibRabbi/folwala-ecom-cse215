009894d8d0ae2acb714949aecfe70c7461212feb
ShahriarRakibRabbi <beingshahriarrakib@gmail.com>
2024-05-24 21:18:17 +0600

    feat: Add model and services classes

---
+++ src/model/Product.java
@@ -0,0 +1,12 @@
+package model;
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    private String category;
+    private boolean isAvailable;
+    private boolean isDeleted;
+   
+    
+}
