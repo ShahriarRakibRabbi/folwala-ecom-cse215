7db34c294729d6582270399d7033b0136c08177f
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-05-24 22:50:40 +0600

    feat: Add OrderItem model

--- src/models/OrderItem.java
+++ src/models/OrderItem.java
@@ -1,4 +1,23 @@
 package models;
+
 public class OrderItem {
-    
+
+  private int orderItemID;
+  private int orderID;
+  private Product product;
+  private int quantity;
+  private double price;
+
+  public OrderItem(int orderItemID, int orderID, Product product, int quantity, double price) {
+    this.orderItemID = orderItemID;
+    this.orderID = orderID;
+    this.product = product;
+    this.quantity = quantity;
+    this.price = price;
+  }
+
+  public int calculateItemTotal() {
+    return quantity * product.getPrice();
+  }
+
 }
