78106d417ae01ccba895b2c7a29a3ce5757f7938
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-05-24 23:09:30 +0600

    feat: Add Payment model

--- src/models/Payment.java
+++ src/models/Payment.java
@@ -1,4 +1,31 @@
 package models;
+
 public class Payment {
-    
+
+  int paymentID;
+  int orderID;
+  String paymentDate;
+  double paymentAmount;
+  String paymentStatus;
+
+  public Payment(int paymentID, int orderID, String paymentDate, double paymentAmount, String paymentStatus) {
+    this.paymentID = paymentID;
+    this.orderID = orderID;
+    this.paymentDate = paymentDate;
+    this.paymentAmount = paymentAmount;
+    this.paymentStatus = paymentStatus;
+  }
+
+  public int getPaymentID() {
+    return paymentID;
+  }
+
+  public int getOrderID() {
+    return orderID;
+  }
+
+  public void processPayment() {
+    this.paymentStatus = "Processed";
+  }
+
 }
